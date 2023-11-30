2450ec83278439e6bd97c803cb4ddb9cffdbbdf9
Tomas <tywe@kth.se>
2023-11-30 15:04:36 +0100

    finish heuristic

--- main.cpp
+++ main.cpp
@@ -102,7 +102,11 @@ vector<tuple<int, int>> deriveEdges(vector<int>& changes, char mode) {
     int end = (mode == 'X') ? changes.size() - 2 : changes.size() - 1;
 
     for (int i = start; i < end; i += 2) {
-        tuple<int, int> edge = make_tuple(changes[i], changes[i + 1]);
+        int a = changes[i];
+        int b = changes[i + 1];
+        int first = a > b ? a:b;
+        int second = a > b ? a:b;
+        tuple<int, int> edge = make_tuple(first, second);
         edges.push_back(edge);
     }
 
@@ -112,28 +116,14 @@ vector<tuple<int, int>> deriveEdges(vector<int>& changes, char mode) {
 vector<tuple<int, int>> deriveEdgesFromTour(vector<int> tour) {
     vector<tuple<int, int>> edges;
     for(int i = 0; i < tour.size(); i++) {
-        tuple<int , int> edge = make_tuple(tour[i], tour[(i+1)%tour.size()]);
+        int a = tour[i];
+        int b = tour[(i+1)%tour.size()];
+        int first = a > b ? a:b;
+        int second = a > b ? a:b;
+        tuple<int , int> edge = make_tuple(first, second);
         edges.push_back(edge);
     }
     return edges;
-}
-
-vector<int> constructNewTour(vector<int> tour, vector<int> changes) {
-    vector<tuple<int, int>> currentEdges = deriveEdgesFromTour(tour);
-
-    vector<tuple<int, int>> X = deriveEdges(changes, 'X');
-    vector<tuple<int, int>> Y = deriveEdges(changes, 'Y');
-
-    for (int i = 0; i < X.size(); i++) {
-        for(int j = 0; j < currentEdges.size(); j++) {
-            if(X[i] == currentEdges[j]) {
-                currentEdges.erase(currentEdges.begin()+j);
-                break;
-            }
-        }
-    }
-    currentEdges.insert(currentEdges.end(), Y.begin(), Y.end());
-    return createTourFromEdges(currentEdges);
 }
 
 vector<int> createTourFromEdges(vector<tuple<int, int>> edges) {
@@ -164,6 +154,26 @@ vector<int> createTourFromEdges(vector<tuple<int, int>> edges) {
     return tour;
 
 }
+
+vector<int> constructNewTour(vector<int> tour, vector<int> changes) {
+    vector<tuple<int, int>> currentEdges = deriveEdgesFromTour(tour);
+
+    vector<tuple<int, int>> X = deriveEdges(changes, 'X');
+    vector<tuple<int, int>> Y = deriveEdges(changes, 'Y');
+
+    for (int i = 0; i < X.size(); i++) {
+        for(int j = 0; j < currentEdges.size(); j++) {
+            if(X[i] == currentEdges[j]) {
+                currentEdges.erase(currentEdges.begin()+j);
+                break;
+            }
+        }
+    }
+    currentEdges.insert(currentEdges.end(), Y.begin(), Y.end());
+    return createTourFromEdges(currentEdges);
+}
+
+
 
 bool isDisjunctive(vector<int> tIndex, int x, int y) {
     if(x == y) {
@@ -285,6 +295,119 @@ int selectNewT(vector<int> tIndex) {
         return -1;
     }
 
+}
+
+int getIndex(int node) {
+    int i = 0;
+    for(int i = 0; i < numCities; i++) {
+        if(tour[i] == node) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int getNearestNode(int index) {
+    double minDistance = numeric_limits<double>::max();
+    int nearestNode = -1;
+    int currentNode = tour[index];
+    for(int i = 0; i < numCities; i++) {
+        if(i != currentNode) {
+            double distance = matrix[currentNode][i];
+            if(distance < minDistance) {
+                nearestNode = getIndex(i);
+                minDistance = distance;
+            }
+        }
+    }
+    return nearestNode;
+
+}
+
+void linkernighan(int t1, int t2, int t3) {
+    vector<int> tIndex;
+    tIndex.push_back(-1);
+    tIndex.push_back(t1);
+    tIndex.push_back(t2);
+    tIndex.push_back(t3);
+    double primeGain = matrix[tour[t2]][tour[t1]] - matrix[tour[t3]][tour[t2]];
+    double gstar = 0;
+    double Gi = primeGain;
+    int k = 3;
+    int i = 4;
+    while(true) {
+        int newT = selectNewT(tIndex);
+        if(newT == -1) {
+            break;
+        }
+
+        tIndex.insert(tIndex.begin()+i, newT);
+        int tiplus1 = getNextPossibleY(tIndex);
+        if(tiplus1 == -1) {
+            break;
+        }
+
+        Gi += matrix[tour[tIndex.size()-2]][tour[newT]];
+        if(Gi - matrix[tour[newT]][tour[t1]] > gstar) {
+            gstar = Gi - matrix[tour[newT]][tour[t1]];
+            k = i;
+        }
+
+        tIndex.push_back(tiplus1);
+        Gi -= matrix[tour[newT]][tour[tiplus1]];
+        i += 2;
+    }
+    if(gstar > 0) {
+        tIndex[k+1] = tIndex[1];
+        vector<int> al2;
+        for(int i = 0; i < k+2; i++) {
+            al2.push_back(tIndex[i]);
+        }
+        tour = constructNewTour(tour, al2);
+    }
+}
+
+void improve() {
+    for(int i = 0; i < numCities; i++) {
+        improve(i);
+    }
+}
+
+void improve(int i) {
+    improve(i, false);
+}
+
+void improve(int t1, bool previous) {
+    int t2 = previous ? getAdjacentIdx(t1, -1) : getAdjacentIdx(t1, 1);
+    int t3 = getNearestNode(t2);
+
+    if(t3 != -1 && matrix[tour[t2]][tour[t3]] < matrix[tour[t1]][tour[t2]]) {
+        linkernighan(t1, t2, t3);
+    }
+    else if(!previous) {
+        improve(t1, true);
+    }
+}
+
+double getDistance(){
+    double sum = 0;
+    for(int i = 0; i < numCities; i++) {
+        int a = tour[i];
+        int b = tour[(i+1)%numCities];
+        sum += matrix[a][b];
+    }
+    return sum;
+}
+
+void runHeuristic() {
+    double oldDist = 0;
+    double newDist = getDistance();
+
+    while(newDist < oldDist) {
+        oldDist = newDist;
+        improve();
+        newDist = getDistance();
+    }
 }
 
 int main() {
